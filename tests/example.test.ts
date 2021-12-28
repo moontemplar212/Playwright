ed4e4c9b8ff643d14feb3c7f3f5302001f93c0c8
moontemplar212 <sjdavies24@hotmail.com>
2021-12-28 12:39:28 +1000

    more tests and before hook

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -1,7 +1,25 @@
 import { test, expect } from '@playwright/test';
 
-test('basic test', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  const title = page.locator('.navbar__inner .navbar__title');
-  await expect(title).toHaveText('Playwright');
+test.describe('feature foo', () => {
+  test.beforeEach(async ({ page }) => {
+
+    await page.goto('https://playwright.dev/');
+  });
+
+  test('basic test', async ({ page }) => {
+    const title = page.locator('.navbar__inner .navbar__title');
+    await expect(title).toHaveText('Playwright');
+  });
+
+  test('my test', async ({ page }) => {
+    await expect(page).toHaveTitle(/Playwright/);
+  
+    await expect(page.locator('text=Get Started').first()).toHaveAttribute('href', '/docs/intro');
+  
+    await expect(page.locator('text=Learn more').first()).toBeVisible();
+  
+    await page.click('text=Get Started');
+  
+    await expect(page.locator('text=Introduction').first()).toBeVisible();
+  });
 });
